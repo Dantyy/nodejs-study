2b16d027564a60f290847029f7268b998116b9a4
Danty Xiao <374889687@xogrp.com>
2019-08-19 17:24:07 +0800

    first try

--- showRegistry.js
+++ showRegistry.js
@@ -1,52 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://qa_registry_feed_readwrite:Test1234@qa-registry-feed-82xp1-mongodb.net/qa-registry-feed');
+mongoose.connect('mongodb://qa_registry_feed_readwrite:Test1234@qa-registry-feed-82xp1-mongodb.net/qa-registry-feed', {useNewUrlParser: true});
 
-//Defining a Model:
-//Models are defined through the Schema interface.
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const schema = new Schema;
+const MyModel = mongoose.model('feedDetailLog', schema);
 
-const BlogPost = new Schema({
-    author: ObjectId,
-    title: String,
-    body: String,
-    date: Date
-});
-
-//The following example shows some of these features:
-const Comment = new Schema({
-    name: { tyoe: String, default: 'hahaha' },
-    age: { type: Number, min: 18, index: true },
-    bio: { type: String, match: /[a-z]/ },
-    date: { tyoe: Date, default: Date.now },
-    buff: Buffer
-});
-//a setter
-Comment.path('name').set(function (v){
-    return capitalize(v);
-});
-//middleware
-Comment.pre('save', function(next){
-    notify(this.get('email'));
-    next();
-});
-
-
-
-const MyModel = mongoose.model('Ticket', mySchema);
-MyModel.find({}, function(err, docs) {
-    // docs.forEach
+MyModel.find({}, {limit:1}, function(err, res) {
+    console.log(res);
 });    //You can also findOne, findById, update, etc. 
 
-const conn = mongoose.createConnection('your connection string');
-const MyModel = conn.model('ModelName', schema);
-const m = new MyModel;
-m.save();  //works
-
-const BlogPost = mongoose.model('BlogPost'); //retrieve my model
-const post = new BlogPost(); //Creare a blog post
-post.comments.push({ title: 'My comment' }); //create a comment
-post.save(function(err){
-    if (!err) console.log('Success!');
-});
-
+app.listen(3000)
