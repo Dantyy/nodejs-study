6362e25acbeda37f9281e8e5c4c07c6c2f21d1fe
Danty Xiao <374889687@xogrp.com>
2019-08-19 18:58:28 +0800

    second

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://qa_registry_feed_readwrite:Test1234@qa-registry-feed-82xp1.mongodb.net/qa-registry-feed', {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+const schema = new Schema;
+const MyModel = mongoose.model('feedDetailLog', schema);
+
 
 app.use(async ctx=>{
-    ctx.body = 'The program is running on port 3000'
+    // ctx.body = 'The program is running on port 3000'
+    console.log("app");
+    MyModel.findOne({}, (err,res) =>{
+        console.log(res);
+        ctx.body = res;
+    })
 });
 
 app.listen(3000);
