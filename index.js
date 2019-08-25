be4f2880df63c316a5d19e69eca8ad7c1dcb049c
Danty Xiao <374889687@qq.com>
2019-08-25 14:09:12 +0800

    have some mistakes

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const Koa = require('koa');
 const app = new Koa();
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://qa_registry_feed_readwrite:Test1234@qa-registry-feed-82xp1.mongodb.net/qa-registry-feed', {useNewUrlParser: true});
-const Router = require("koa-router");
-const router = new Router;
+const mongoose = require('mongoose');
+const router = require("koa-router");
+const bodyParser = require("koa-bodyParser");
+mongoose.connect('mongodb://localhost/testDB', {useNewUrlParser: true});
 
-// const Schema = mongoose.Schema;
-// const schema = new Schema;
+
+app.use(router.routes())
+   .use(router.allowedMethods())
+   .use(bodyParser());
+
+router.post('api/create', async ctx=>{
+    const name = ctx.query.name;
+    ctx.body={
+        status:"success",
+        name: name
+    };
+});
+app.listen(3000);
+
+
+
+// const schema = mongoose.Schema;
 // const MyModel = mongoose.model('feedDetailLog', schema, 'feedDetailLog');
 
-app.use(router.routes());
-router.get('/api/registry/:id', async (ctx) =>{
-    const id = ctx.params.id;
+// router.get('/api/registry/:id', async (ctx) =>{
+//     const id = ctx.params.id;
     //获取参数
     // const firstName = ctx.query.first_name;
     // const lastName = ctx.query.last_name;
     
     //将获取的参数从接口返回
-    ctx.body = {
-        status: 'success',
-        id: id,
-        // data: {
-        //     firstName,
-        //     lastName,
-        // },
-    };
+    // ctx.body = {
+    //     status: 'success',
+    //     id: id,
+    //     // data: {
+    //     //     firstName,
+    //     //     lastName,
+    //     // },
+    // };
     // const res = await MyModel.findOne({});
     // ctx.body = res;
-});
-
-
-app.listen(3000);
+// });
