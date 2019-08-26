bb34ed552aef601d2c331d36ac8c3262f9195bab
Danty Xiao <374889687@qq.com>
2019-08-26 23:09:01 +0800

    need help

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
 const mongoose = require('mongoose');
-const router = require("koa-router");
-const bodyParser = require("koa-bodyParser");
+const Router = require("koa-router");
+const router = new Router();
+const bodyParser = require("koa-bodyparser");
 mongoose.connect('mongodb://localhost/testDB', {useNewUrlParser: true});
 
 
-app.use(router.routes())
-   .use(router.allowedMethods())
-   .use(bodyParser());
+app.use(bodyParser())
+   .use(router.routes())
+   .use(router.allowedMethods());
 
-router.post('api/create', async ctx=>{
+router.post('/api/create/', async ctx=>{
     const name = ctx.query.name;
+    //如何插入到mongoDB??
+
     ctx.body={
         status:"success",
         name: name
@@ -21,8 +24,8 @@ app.listen(3000);
 
 
 
-// const schema = mongoose.Schema;
-// const MyModel = mongoose.model('feedDetailLog', schema, 'feedDetailLog');
+// const Schema = mongoose.Schema;
+// const MyModel = mongoose.model('testDB', schema, 'testDB');
 
 // router.get('/api/registry/:id', async (ctx) =>{
 //     const id = ctx.params.id;
